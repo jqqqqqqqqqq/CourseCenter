d3fc5e755f1f27742ea96a53b6e9f1d4708d6ba4
张敏捷 <502982165@qq.com>
2017-06-27 17:46:22 +0800

    display semester_info

--- app/main/views.py
+++ app/main/views.py
@@ -15,18 +15,13 @@ def index():
 
 @main.route('/manage-semester', methods=['GET', 'POST'])
 def manage_semester():
-    form = EditSemesterForm()
-    if form.validate_on_submit():
-        semester_id = int(form.id.data)
-        semester_base_info = form.base_info.data
-        time = form.time.data
-        semester_begin_time, end_time = time.split('-')
-        start_time = datetime.datetime.strptime(begin_time, '%m/%d/%Y')
-        finish_time = datetime.datetime.strptime(end_time, '%m/%d/%Y')
-        semester = Semester(id=semester_id, base_info=semester_base_info, begin_time=start_time, end_time=finish_time)
-        db.session.add(semester)
-        return redirect('.manage-semester')
-    return render_template('manage_semester.html', form=form)
+    semester_list = Semester.query.all()
+    res = []
+    for semester in semester_list:
+        res.append({'id': semester.id,
+                    'base_info': semester.base_info,
+                    'time': semester.begin_time + '-' + semester.end_time})
+    return render_template('manage_semester.html', res=res)
 
 
 @main.route('/manage-course')
