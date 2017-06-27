63a2b96d5e0251f09bbd35c47b22d04e97c9dfc6
MoonDropX <minalinskyx@hotmail.com>
2017-06-27 22:51:32 +0900

    Edit login view and template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user
 from .forms import LoginForm
 from . import auth
@@ -24,5 +24,6 @@ def login():
             if user is not None and user.verify_password(form.password.data):
                 login_user(user, form.remember_me.data)
                 return redirect(request.args.get('next') or url_for('main.index'))
+        flash('用户名或账户错误')
 
     return render_template('auth/login.html', form=form)
--- manage.py
+++ manage.py
@@ -2,18 +2,26 @@ import os
 
 from app import create_app, db
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand, upgrade
 from app.models.models import Student, Teacher, DeanInfo, Semester, Team, TeamMember, Homework ,Submission, Attachment, Course, CourseTime, TeacherTeam
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+
 def make_shell_context():
     return dict(app=app, db=db, Student=Student)
 manager.add_command('shell', Shell(make_context=make_shell_context))
-
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def deploy():
+    """部署数据库"""
+    upgrade()
+    DeanInfo.init_dean()
+
 
 if __name__ == '__main__':
     manager.run()
