fe8c434056e594772979c3871d91dd9d6c371a19
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-06-27 16:22:16 +0800

    add set_course_info

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from . import main
+from .forms import CourseForm
+
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -12,6 +14,17 @@ def index():
 def manage_semester():
     return render_template('manage_semester.html')
 
+
 @main.route('/manage-course')
 def manage_course():
     return render_template('manage_course.html')
+
+
+@main.route('/set-course-info')
+def set_course_info():
+    form = CourseForm()
+    form.teamsize.data = 1
+    if form.validate_on_submit():
+        print(form.course_info.data)
+        return redirect(request.args.get('next') or url_for(''))
+    return render_template('set_course_info.html', form=form)
