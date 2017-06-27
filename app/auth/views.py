0df5d9685f51a1b19535571417303e2ec5f214b4
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-06-27 15:04:11 +0800

    bug fix

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,20 +14,19 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.identity.data == '0':
-            user = DeanInfo.query.filter_by(id=form.username).first()
-            if user is not None and user.verify_password(form.password):
+            user = DeanInfo.query.filter_by(id=form.username.data).first()
+            if user is not None and user.verify_password(form.password.data):
                 login_user(user, form.remember_me.data)
-                return redirect(request.args.get('next') or url_for(''))
+                return redirect(request.args.get('next') or url_for('main.index'))
         elif form.identity.data == '1':
-            user = Teacher.query.filter_by(id=form.username).first()
-            if user is not None and user.verify_password(form.password):
+            user = Teacher.query.filter_by(id=form.username.data).first()
+            if user is not None and user.verify_password(form.password.data):
                 login_user(user, form.remember_me.data)
-                return redirect(request.args.get('next') or url_for(''))
+                return redirect(request.args.get('next') or url_for('main.index'))
         else:
-            user = Student.query.filter_by(id=form.username).first()
-            print(form.username.data)
-            if user is not None and user.verify_password(form.password):
+            user = Student.query.filter_by(id=form.username.data).first()
+            if user is not None and user.verify_password(form.password.data):
                 login_user(user, form.remember_me.data)
-                return redirect(request.args.get('next') or url_for(''))
+                return redirect(request.args.get('next') or url_for('main.index'))
 
     return render_template('auth/login.html', form=form)
