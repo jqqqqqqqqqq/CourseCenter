003d3d5ffedc92c62431932f66ed5a870b986fe6
xq <736565860@qq.com>
2017-06-27 16:36:23 +0800

    modify main.views

--- app/main/views.py
+++ app/main/views.py
@@ -16,19 +16,16 @@ def index():
 @main.route('/manage-semester', methods=['GET', 'POST'])
 def manage_semester():
     form = EditSemesterForm()
-    id = int(form.id.data)
-    base_info = form.base_info.data
-    time = form.time.data
-    begin_time, end_time = time.split('-')
-    start_time = datetime.datetime.strptime(begin_time, '%m/%d/%Y')
-    finish_time = datetime.datetime.strptime(end_time, '%m/%d/%Y')
     if form.validate_on_submit():
-        semester = Semester(id=id, base_info=base_info, begin_time=start_time, end_time=finish_time)
+        semester_id = int(form.id.data)
+        semester_base_info = form.base_info.data
+        time = form.time.data
+        semester_begin_time, end_time = time.split('-')
+        start_time = datetime.datetime.strptime(begin_time, '%m/%d/%Y')
+        finish_time = datetime.datetime.strptime(end_time, '%m/%d/%Y')
+        semester = Semester(id=semester_id, base_info=semester_base_info, begin_time=start_time, end_time=finish_time)
         db.session.add(semester)
         return redirect('.manage-semester')
-    form.id.data = id
-    form.base_info.data = base_info
-    form.time.data = time
     return render_template('manage_semester.html', form=form)
 
 
