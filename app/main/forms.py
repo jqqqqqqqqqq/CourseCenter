775c887f596557917e596038f408100b90e6e1fd
MoonDropX <minalinskyx@hotmail.com>
2017-07-01 02:13:37 +0900

    删除了完全没用到的SemesterForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,13 +8,6 @@ from flask_uploads import UploadSet
 upsr = UploadSet('files', extensions=('xls', 'xlsx', 'pdf', 'doc', 'docx', 'txt', 'zip', '7z', 'rar'))
 
 
-class SemesterForm(FlaskForm):
-    id = IntegerField('学期ID', validators=[DataRequired()])
-    base_info = TextAreaField('学期基本信息', validators=[])
-    begin_time = DateField('开始周', validators=[DataRequired()])
-    end_time = DateField('结束周', validators=[DataRequired()])
-
-
 class UploadResourceForm(FlaskForm):
     up = FileField(validators=[
         FileAllowed(upsr, u'xls, xlsx, pdf, doc, docx, txt, zip, 7z, rar'),
