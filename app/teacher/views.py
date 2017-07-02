747a55ab59f662bb4f304cf7cd35a356557b7047
MoonDropX <minalinskyx@hotmail.com>
2017-07-02 17:07:34 +0900

    修改教师作业页面的显示

--- app/teacher/views.py
+++ app/teacher/views.py
@@ -128,11 +128,7 @@ def set_homework(course_id):
         begin_time = datetime.strptime(begin_time, '%m/%d/%Y %H:%M')
         end_time = datetime.strptime(end_time, '%m/%d/%Y %H:%M')
 
-        # 可能是edit或者是add
-        if request.args.get('action') == 'edit':
-            homework = Homework.query.filter_by(id=request.args.get('homework_id')).first()
-        else:
-            homework = Homework()
+        homework = Homework()
 
         homework.name = form.name.data
         homework.base_requirement = form.base_requirement.data
@@ -145,32 +141,52 @@ def set_homework(course_id):
         db.session.add(homework)
         db.session.commit()
 
-        if request.args.get('homework_id'):
-            flash('修改成功！', 'success')
-            return redirect(url_for('teacher.set_homework',
-                                    course_id=course_id,
-                                    action='show',
-                                    homework_id=request.args.get('homework_id')))
         flash('发布成功！', 'success')
         return redirect(url_for('teacher.set_homework', course_id=course_id))
     course = Course.query.filter_by(id=course_id).first()
-    if request.args.get('action') == 'show':
-        # 作业详情
-        homework = Homework.query.filter_by(id=request.args.get('homework_id')).first()
-        form.name.data = homework.name
-        form.base_requirement.data = homework.base_requirement
-        form.time.data = '{} - {}'.format(homework.begin_time.strftime('%m/%d/%Y %H:%M'),
-                                          homework.end_time.strftime('%m/%d/%Y %H:%M'))
-        form.weight.data = homework.weight
-        form.max_submit_attempts.data = homework.max_submit_attempts
 
-        return render_template('teacher/homework_detail.html',
-                               course_id=course_id,
-                               course=course,
-                               form=form,
-                               homework=homework)
     homework_list = Homework.query.filter_by(course_id=course_id).all()
     return render_template('teacher/homework.html', course_id=course_id, homeworks=homework_list, form=form, course=course)
+
+
+@teacher.route('/<course_id>/homework/<homework_id>', methods=['GET', 'POST'])
+@UserAuth.teacher_course_access
+def homework_detail(course_id, homework_id):
+    # 作业详情
+    form = HomeworkForm()
+    course = Course.query.filter_by(id=course_id).first()
+    homework = Homework.query.filter_by(id=homework_id).first()
+    if form.validate_on_submit():
+        # 修改作业
+        begin_time, end_time = form.time.data.split(' - ')
+        begin_time = datetime.strptime(begin_time, '%m/%d/%Y %H:%M')
+        end_time = datetime.strptime(end_time, '%m/%d/%Y %H:%M')
+
+        homework.name = form.name.data
+        homework.base_requirement = form.base_requirement.data
+        homework.begin_time = begin_time
+        homework.end_time = end_time
+        homework.weight = form.weight.data
+        homework.max_submit_attempts = form.max_submit_attempts.data
+        homework.course_id = course_id
+
+        flash('修改成功！', 'success')
+        return redirect(url_for('teacher.homework_detail',
+                                course_id=course_id,
+                                homework_id=homework_id))
+
+    form.name.data = homework.name
+    form.base_requirement.data = homework.base_requirement
+    form.time.data = '{} - {}'.format(homework.begin_time.strftime('%m/%d/%Y %H:%M'),
+                                      homework.end_time.strftime('%m/%d/%Y %H:%M'))
+    form.weight.data = homework.weight
+    form.max_submit_attempts.data = homework.max_submit_attempts
+
+    return render_template('teacher/homework_detail.html',
+                           course_id=course_id,
+                           course=course,
+                           form=form,
+                           homework=homework)
 
 
 @teacher.route('/uploadresource', methods=['GET', 'POST'])
